858f2fe35ac3d030e122d5ab47991f7dfb35942b
Nihith <nihithsaip@gmail.com>
2017-03-25 11:31:58 +0530

    Update q9.cpp

--- q9.cpp
+++ q9.cpp
@@ -1,27 +1,25 @@
 #include <stdio.h>
 int main
 {
-  int a[10],n,i,first=0,second=0;
-  printf("entert the no of elements");
-  scanf("%d",&n);
-  for(i=0;i<n;i++)
-  {
-  scanf("%d",&a[i]);
-  if(a[i]%2==0)
-  {
-    if(a[i]>first)
-    {
-      first=a[i];
-    }
-  }
+int n,i,a[10],first=0,second=0;;
+printf("Enter no of elements");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
 }
-  for(i=0;i<n;i++)
-  {
-   if(a[i]!=first&&a[i]>second&&a[i]%2==0)
-   {
-     second=a[i];
-   }
-  }
-  printf("%d",second);
-	return 0;
+for(i=0;i<n;i++)
+{
+if(first<a[i])
+{
+second=first;
+first=a[i];
+}
+else if(second<a[i])
+{
+second=a[i];
+}
+}
+printf("%d",second);
+return 0;
 }
